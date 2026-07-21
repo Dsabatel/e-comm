30c79d65abc2879579d638bb332d524869309565
Diego Sabatel <sabateldiego@gmail.com>
2026-07-21 16:52:32 -0300

    added: add product with image

--- src/main/java/com/telusko/springecom/model/Product.java
+++ src/main/java/com/telusko/springecom/model/Product.java
@@ -1,10 +1,7 @@
 package com.telusko.springecom.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,7 +17,7 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
     private String name;
     private String description;
     private String brand;
@@ -30,5 +27,9 @@ public class Product {
     private Date releaseDate;
     private boolean productAvailable;
     private int stockQuantity;
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageData;
 
 }
